d7cb203dc4d7a724c31b7d65bd9069dba6efed78
DavidjFilho <david_curso@hotmail.com>
2023-10-26 21:11:29 -0300

    use middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
-import express from "express";
+import express, { Application } from "express";
 import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
 require("dotenv").config();
 
-const app = express();
-//db
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //router
-//middleware
 
 mongoose.connect(
   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jcgpdjt.mongodb.net/${process.env.DB_DATABASE}`,
