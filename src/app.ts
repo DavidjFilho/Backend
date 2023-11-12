c8ecf6e31311b63720fe2e83417b49002da4c271
LuizHSGonzaga <lurique.pro@gmail.com>
2023-11-11 22:19:36 -0300

    Att

--- src/app.ts
+++ src/app.ts
@@ -8,43 +8,43 @@ import { errorMiddleware } from "./middlewares/ErrorMiddlewares";
 require("dotenv").config();
 
 class App {
-    public app: Application;
+	public app: Application;
 
-    public constructor() {
-        this.start();
-        this.database();
-        this.middlewaresPreRoute();
-        this.routes();
-        this.middlewaresPosRoute();
-    }
+	public constructor() {
+		this.start();
+		this.database();
+		this.middlewaresPreRoute();
+		this.routes();
+		this.middlewaresPosRoute();
+	}
 
-    private start(): void {
-        this.app = express();
-    }
+	private start(): void {
+		this.app = express();
+	}
 
-    private middlewaresPreRoute(): void {
-        this.app.use(express.json());
-        this.app.use(cors());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-    }
-    private database(): void {
-        if (process.env.NODE_AMBIENT === "0") {
-            //TEST
-            mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_DATABASE_TEST}`, {});
-        } else if (process.env.NODE_AMBIENT === "1") {
-            //DEV
-            mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_DATABASE_DEV}`, {});
-        } else if (process.env.NODE_AMBIENT === "2"){
-            //PROD
-            mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_DATABASE_PROD}`, {});
-        } 
-    }
-    private routes(): void {
-        this.app.use(routes);
-    }
-    private middlewaresPosRoute(): void {
-        this.app.use(errorMiddleware);
-    }
+	private middlewaresPreRoute(): void {
+		this.app.use(express.json());
+		this.app.use(cors());
+		this.app.use(bodyParser.urlencoded({ extended: false }));
+	}
+	private database(): void {
+		if (process.env.NODE_AMBIENT === "0") {
+			//TEST
+			mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_DATABASE_TEST}`, {});
+		} else if (process.env.NODE_AMBIENT === "1") {
+			//DEV
+			mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_DATABASE_DEV}`, {});
+		} else if (process.env.NODE_AMBIENT === "2") {
+			//PROD
+			mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.DB_DATABASE_PROD}`, {});
+		}
+	}
+	private routes(): void {
+		this.app.use(routes);
+	}
+	private middlewaresPosRoute(): void {
+		this.app.use(errorMiddleware);
+	}
 }
 
 export default new App().app;
--- src/services/UserServices.faker.ts
+++ src/services/UserServices.faker.ts
@@ -4,31 +4,31 @@ import { IUser, IUserCreate } from "../interfaces/IUser";
 import MongoUserRepository from "../repositories/MongoUserRepository";
 
 const generateUser = async (): Promise<IUser> => {
-    let email: string;
-    do {
-        email = faker.internet.email();
-    } while (await MongoUserRepository.findByObject({ email }));
+	let email: string;
+	do {
+		email = faker.internet.email();
+	} while (await MongoUserRepository.findByObject({ email }));
 
-    return {
-        id: faker.string.uuid(),
-        name: faker.internet.userName(),
-        email,
-        password: md5(faker.string.alphanumeric(8) + process.env.SALT_KEY),
-        status: 1,
-    };
+	return {
+		id: faker.string.uuid(),
+		name: faker.internet.userName(),
+		email,
+		password: md5(faker.string.alphanumeric(8) + process.env.SALT_KEY),
+		status: 1,
+	};
 };
 
 const generateUserCreate = async (): Promise<IUserCreate> => {
-    let email: string;
-    do {
-        email = faker.internet.email();
-    } while (await MongoUserRepository.findByObject({ email }));
+	let email: string;
+	do {
+		email = faker.internet.email();
+	} while (await MongoUserRepository.findByObject({ email }));
 
-    return {
-        name: faker.internet.userName(),
-        email,
-        password: faker.string.alphanumeric(8)+ "L@8",
-    };
+	return {
+		name: faker.internet.userName(),
+		email,
+		password: faker.string.alphanumeric(8) + "L@8a",
+	};
 };
 
 export { generateUser, generateUserCreate };
